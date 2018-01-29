1983c0535a7b09b94fe3618754df26021792abef
listenviolet <listenviolet@gmail.com>
2018-01-29 10:50:13 +0800

    A + B solved

--- oj/1002-A+B-problem.cpp
+++ oj/1002-A+B-problem.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 using namespace std;
 
-#define maxn 15
+#define maxn 22
 
 void disArr(int* c, int len_c)
 {
@@ -22,7 +22,7 @@ void add(int* a, int len_a, int* b, int len_b)
         for(int i = 0; i < len_a; ++i)
         {
             c[i] = (a[i] + b[i] + cnt)%10;
-            cnt = (a[i] + b[i]) / 10;
+            cnt = (a[i] + b[i] + cnt) / 10;
 
         }
         for(int i = len_a; i < len_b; ++i)
@@ -40,7 +40,7 @@ void add(int* a, int len_a, int* b, int len_b)
         for(int i = 0; i < len_b; ++i)
         {
             c[i] = (a[i] + b[i] + cnt)%10;
-            cnt = (a[i] + b[i]) / 10;
+            cnt = (a[i] + b[i] + cnt) / 10;
         }
 
         for(int i = len_b; i < len_a; ++i)
@@ -59,7 +59,7 @@ void add(int* a, int len_a, int* b, int len_b)
     cout<<endl;
 }
 
-void minusAB(int* a, int len_a, int* b, int len_b)
+void minusAB(int* a, int len_a, int* b, int len_b)  //big - small
 {
     int i = 0, j = 0, cnt = 0,len_c = 0;
     int c[maxn];
@@ -76,23 +76,16 @@ void minusAB(int* a, int len_a, int* b, int len_b)
 
     if(len_a == len_b)
     {
-        len_c = len_a;
+        int k = 0; len_c = len_a;
+        while(c[len_a - 1 - k] == 0)
+        {
+            len_c--;
+            k++;
+        }
         if(cnt) cout << "-";
         disArr(c,len_c);
     }
-/*
-    else if(len_a < len_b)
-    {
-        len_c = len_b;
-        for(int k = len_a; k < len_b; ++k)
-        {
-            c[k] = 10 - cnt - b[k];
-            cnt = 0;
-        }
-        cout << "-";
-        disArr(c, len_c);
-    }
-*/
+
     else if(len_a > len_b)
     {
         if(len_a > len_b + 1)
@@ -111,7 +104,6 @@ void minusAB(int* a, int len_a, int* b, int len_b)
             if(c[len_b] == 0)
             {
                 len_c = len_b;
-                cout << "-";
             }
             else len_c = len_a;
             disArr(c, len_c);
@@ -135,11 +127,25 @@ int* transToInt(char* s, int len)
         for(int i = 0; i < len; ++i)
         {
             tmp[i] = s[len - i - 1] - '0';
-            //cout<<tmp[i];
         }
     }
-    //cout <<endl;
     return tmp;
+}
+
+int cmp(int* a, int len_a, int* b, int len_b)
+{
+    int flag = 0;
+    if(len_a > len_b) return 1; //a > b
+    else if(len_a < len_b) return -1; // a < b
+    else if(len_a == len_b)
+    {
+        for(int i = len_a; i >= 0 ; --i)
+        {
+            if(a[i] > b[i]) return flag = 1;
+            else if(a[i] < b[i]) return flag = -1;
+        }
+        if(flag == 0) return 0; //a = b
+    }
 }
 
 int main()
@@ -147,50 +153,34 @@ int main()
     char s1[maxn],s2[maxn];
     while(scanf("%s%s", s1,s2) != EOF)
     {
-        //two positive int
-        if(s1[0] != '-' && s2[0] != '-')
+        int len_a , len_b;
+        int *a, *b;
+        len_a = (s1[0] == '-') ? strlen(s1) - 1 : strlen(s1);
+        len_b = (s2[0] == '-') ? strlen(s2) - 1 : strlen(s2);
+        a = transToInt(s1, strlen(s1));
+        b = transToInt(s2, strlen(s2));
+        int flag = cmp(a, len_a, b, len_b);
+        if(flag == 0) {cout << "0" <<endl;}
+        else if(s1[0] != '-' && s2[0] != '-')
         {
-            int len_a = strlen(s1);
-            int len_b = strlen(s2);
-            int *a, *b;
-            a = transToInt(s1, len_a);
-            b = transToInt(s2, len_b);
             add(a, len_a, b, len_b);
         }
-
+        else if(s1[0] != '-' && s2[0] =='-')
+        {
+            if(flag == 1) { minusAB(a, len_a, b, len_b);}
+            //else if(flag == 0) {cout << "0" <<endl;}
+            else if(flag == -1) {cout << "-"; minusAB(b, len_b, a, len_a);}
+        }
         else if(s1[0] == '-' && s2[0] != '-')
         {
-            int len_a = strlen(s2); //cout << len_a <<endl;
-            int len_b = strlen(s1) - 1; //cout << len_b <<endl;
-            int *a, *b;
-            a = transToInt(s2, len_a);
-            b = transToInt(s1, len_b + 1);
-            minusAB(a, len_a, b, len_b);
+            if(flag == 1) {cout << "-"; minusAB(a, len_a, b, len_b);}
+            else if(flag == -1)  {minusAB(b, len_b, a, len_a);}
         }
-
-        else if(s1[0] != '-' && s2[0] == '-')
-        {
-            int len_a = strlen(s1)-1; //cout << len_a <<endl;
-            int len_b = strlen(s2); //cout << len_b <<endl;
-            int *a, *b;
-            a = transToInt(s2, len_a + 1);
-            b = transToInt(s1, len_b);
-            if(len_a < len_b) minusAB(b, len_b, a, len_a);
-            else minusAB(a, len_a, b, len_b);
-        }
-
         else if(s1[0] == '-' && s2[0] == '-')
         {
-            int len_a = strlen(s1)-1;
-            int len_b = strlen(s2)-1;
-            int *a, *b;
-            a = transToInt(s2, len_a + 1);
-            b = transToInt(s1, len_b + 1);
-            cout<<"-";
+            cout << "-";
             add(a, len_a, b, len_b);
         }
-
     }
-
     return 0;
 }
