b7b7742f99e6e25de15cbb72028dd3648e0ec808
listenviolet <listenviolet@gmail.com>
2018-01-31 15:12:26 +0800

    high accuracy multiply

---
+++ oj/poj-1001-Exponentiation-answer-intro.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool standardizeNumNoDot(string &s)                        //对原字符串直接修改，删去 前后无意义的0 和 整数后面的'.' 返回原字符串有无小数点
+{
+    while (!s.empty() && '0' == s[0]) s.erase(s.begin());  //去掉开头无意义的0 ：001230
+    if (s.empty()) s = "0";//防止n==1的时候，要输出0       //000000    return true -- 无点
+    bool notDot = true;                                    //notDot:没有点，默认为真
+    for (unsigned i = 0; i < s.size() && notDot; i++)      //若有点，则终止
+    {
+        if ('.' == s[i]) notDot = false;
+    }
+    if (notDot) return true;                               //没有点
+
+    while (!s.empty() && '0' == s[s.size()-1]) s.erase(s.end()-1);   //有点的情况下，从末尾擦去无意义的0： 12.100 ->12.1  12.000 -> 12.
+    if (!s.empty() && '.' == s[s.size()-1]) s.erase(s.end()-1);      //擦去整数后面的点 12. -> 12   . - > empty (点前后有若干0，从前、后擦去0后，只剩'.')
+    if ( s.empty() ) s = "0";                                        //若为空，证明原为0  return false --有点？
+    return false;
+}
+
+int handleDecimalPoint(string &s)                          //删去小数点后的s，返回原来s小数点后有几位
+{
+    if (standardizeNumNoDot(s)) return 0;                  //若notDot = true 没有点，返回0
+    int fraction = 0;
+    int j = 0;
+    for (unsigned i = 0; i < s.size() ; i++)
+    {
+        if (fraction > 0) fraction++;
+        if (s[i] != '.') s[j++] = s[i];                    //去掉小数点后的s 即跳过'.'，后面字符前移
+        else fraction++;
+    }
+    s.erase(s.end()-1);                                    //删去最后一个字符：去掉小数点后，字符前移，最后一个字符多余
+    return fraction - 1;                                   //返回小数点后面的位数
+}
+
+string mulStr(string a, string b)                          //a * b
+{
+    if ("0" == a || "0" == b) return "0";
+    int ap = handleDecimalPoint(a);
+
+    int bp = handleDecimalPoint(b);
+
+    string ans(a.size()+b.size(), '0');
+    for (int i = a.size() - 1; i >= 0 ; i--)
+    {
+        int carry = 0;
+        int an = a[i] - '0';
+        for (int j = b.size() - 1; j >= 0 ; j--)
+        {
+            int bn = b[j] - '0';
+            int sum = an * bn + carry + ans[i+j+1] - '0';
+            carry = sum / 10;
+            ans[i+j+1] = sum % 10 + '0';
+        }
+        if (carry) ans[i] += carry;
+    }
+    if (ap > 0 || bp > 0) ans.insert(ans.end() - ap - bp, '.');
+    standardizeNumNoDot(ans);
+    return ans;
+}
+
+string sPow(string s, int n)
+{
+    if (s.empty() || "0" == s) return "0";//为了程序的健壮性，一定要加上
+    if (0 == n) return "1";
+    if (1 == n) return s;
+
+    string divideStr = sPow(s, n/2);
+    divideStr = mulStr(divideStr, divideStr);
+    if (n % 2) divideStr = mulStr(divideStr, s);
+    return divideStr;
+}
+
+void Exponentiation()
+{
+    string s;
+    int n;
+    while(cin>>s>>n)
+    {
+        standardizeNumNoDot(s);//当n==1的时候
+        cout<<sPow(s, n)<<endl;
+    }
+}
+
+int main()
+{
+    Exponentiation();
+    return 0;
+}
--- oj/poj-1001-Exponentiation.cpp
+++ oj/poj-1001-Exponentiation.cpp
@@ -6,7 +6,8 @@ using namespace std;
 #define maxn 10
 #define max_arr 200
 
-int tmp[maxn];
+int tmp[maxn];              // record the string to int
+int multi_result[max_arr];  // record the int multiply result
 
 int loc(string s)
 {
@@ -14,12 +15,28 @@ int loc(string s)
     {
         if(s[i] == '.') return i;  //location of "." in string index
     }
+    return -1; //ÕûÊıÃ»ÓĞĞ¡Êıµã
 }
 
 int transToInt(string s)
 {
     /* delete the point */
     int locpoint = loc(s);
+    int for_fir = 0;
+    int index_arr = 0;
+
+    if(locpoint == -1)  //ÕûÊı--²»¼Óµã
+    {
+        while(s[for_fir] == '0' && for_fir < s.size()) for_fir++;
+        if(for_fir == s.size()) {tmp[0] = 0; return 1;}
+        while(index_arr < s.size() - for_fir)
+        {
+            tmp[index_arr] = s[s.size() - 1 - index_arr] - '0';
+            index_arr++;
+        }
+        return index_arr;
+    }
+
     int len_new = s.size() - 1;
     for(int i = locpoint; i < len_new; ++i)
     {
@@ -27,34 +44,44 @@ int transToInt(string s)
     }
 
     /* without 0 */
-    int for_fir = 0;
     int back_fir = len_new - 1;
-    int index_arr = 0;
     int len_arr;
 
     while(s[for_fir] == '0' && for_fir < len_new) for_fir++;
     while(s[back_fir] == '0' && back_fir >=0) back_fir--;
 
-    if(for_fir == len_new) {tmp[0] = 0; return 1;}
+    if(for_fir == len_new) {tmp[0] = 0; return 1;}  //È«0
     else
     {
-        int len_arr = back_fir - for_fir + 1;
+        int index_end = (back_fir < locpoint) ? locpoint - 1 : back_fir;
+        //int len_arr = back_fir - for_fir + 1;
+        int len_arr = index_end - for_fir + 1;
         while(index_arr < len_arr)
         {
-            tmp[index_arr] = s[back_fir - index_arr] - '0';
+            //tmp[index_arr] = s[back_fir - index_arr] - '0';
+            tmp[index_arr] = s[index_end - index_arr] - '0';
             index_arr++;
         }
         return len_arr;
     }
 }
 
-int* multiply(int* a, int len_arr, int n) //²»¿ÉÒÔÓÃmultiÖ¸ÏòresultÊı×é£¬¶şÕß½«Í¬Ê±±ä»¯
+int multiply(int* a, int len_arr, int n) //²»¿ÉÒÔÓÃmultiÖ¸ÏòresultÊı×é£¬¶şÕß½«Í¬Ê±±ä»¯
 {
-    int multi[maxn], result[maxn];
+    int multi[max_arr], result[max_arr];
     for(int i = 0; i < len_arr; ++i)
         multi[i] = a[i];
     int len_multi = len_arr;
     int len_result;
+
+    if(n == 1)
+    {
+        for(int i = 0; i < len_arr; ++i)
+        {
+            multi_result[i] = a[i];
+        }
+        return len_arr;
+    }
 
     while(n >= 2)
     {
@@ -64,21 +91,16 @@ int* multiply(int* a, int len_arr, int n) //²»¿ÉÒÔÓÃmultiÖ¸ÏòresultÊı×é£¬¶şÕß½«Í
             for(int j = 0; j < len_multi; ++j)
             {
                 result[i + j] += a[i] * multi[j];
-                cout << a[i] << " * " << multi[j] << " = "<<result[i+j] << " ";
             }
         }
-        cout <<endl;
-        cout << "result:" << endl;
-        for(int i = len_arr + len_multi - 2; i >= 0; --i)
-            cout << i << ": " << result[i] << " ";
-        cout <<endl;
-
         int ncarry = 0;
         for(int i = 0; i < len_arr + len_multi - 1; ++i)
         {
-            result[i] = (result[i] + ncarry) %10;
-            ncarry = (result[i] + ncarry) / 10;
+            int c = result[i] + ncarry;
+            result[i] = c %10;
+            ncarry = c / 10;
         }
+
         len_result = len_arr + len_multi - 1;
         if(ncarry) { result[len_result] = ncarry; len_result++; }
 
@@ -90,7 +112,62 @@ int* multiply(int* a, int len_arr, int n) //²»¿ÉÒÔÓÃmultiÖ¸ÏòresultÊı×é£¬¶şÕß½«Í
         n--;
     }
 
-    return result;
+    for(int i = 0; i < len_result; ++i)
+    {
+        multi_result[i] = result[i];
+    }
+
+    return len_result;
+}
+
+void addDot(int* a, int len, int loc, int n) // loc : Ğ¡ÊıµãºóÃæÓĞ¼¸Î»
+{
+    int new_loc = n * loc;
+    int back_first = 0;
+    while(a[back_first] == 0) back_first++;
+    if(new_loc <= back_first) // ÕûÊı
+    {
+        for(int i = len - 1; i >= new_loc; --i)
+            cout << a[i];
+        cout << endl;
+    }
+    else if (new_loc <= len - 1) //new_loc > back_first  x.xxxx xx.x
+    {
+        for(int i = len - 1; i >= new_loc; --i)
+        {
+            cout << a[i];
+        }
+        cout << ".";
+        for(int i = new_loc - 1; i >= back_first; --i)
+        {
+            cout << a[i];
+        }
+        cout << endl;
+    }
+    else if(new_loc >= len)
+    {
+        cout << ".";
+        for(int i = new_loc - 1; i >= len; --i)
+        {
+            cout << "0";
+        }
+        for(int i = len - 1; i >= back_first; --i)
+        {
+            cout << a[i];
+        }
+        cout << endl;
+    }
+}
+
+int reLoc(string s)
+{
+    int back_zero = s.size() - 1;
+    while(back_zero >= loc(s) && s[back_zero] == '0')
+    {
+        back_zero--;
+    }
+
+    return back_zero - loc(s);
 }
 
 int main()
@@ -100,12 +177,14 @@ int main()
     while(cin >> s >> n)
     {
         int len_arr = transToInt(s);
-        /*
-        for(int i = len_arr - 1; i >= 0; --i)
-            cout << tmp[i];
-        cout <<endl;
-        */
-        int *multi;
-        multi = multiply(tmp, len_arr, n);
+        if(len_arr == 1 && tmp[0] == 0) cout << "0" << endl;
+        else if(n == 0) cout << "1" << endl;
+        else
+        {
+            int len_result = multiply(tmp, len_arr, n); // record in multi_result
+            int re_loc = (loc(s) == -1) ? 0 : reLoc(s);
+            addDot(multi_result, len_result, re_loc, n);
+        }
     }
+    return 0;
 }
