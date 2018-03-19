66c96be81b234f19e4bf54b0b6f55f537fbbf9b9
listenviolet <listenviolet@gmail.com>
2018-03-19 11:38:31 +0800

    dict{sting-list}

---
+++ leetcode/049-Group-Anagrams-prime.py
@@ -0,0 +1,6 @@
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103];  
