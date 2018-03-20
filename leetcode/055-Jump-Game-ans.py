ad04b1bb3682329b69646e6f272331cbb9b9119d
listenviolet <listenviolet@gmail.com>
2018-03-20 10:21:59 +0800

    ans store true or false for i -> (n-1) - Time Limit Exceeded

--- leetcode/055-Jump-Game-ans.py
+++ leetcode/055-Jump-Game-ans.py
@@ -13,8 +13,12 @@ class Solution:
         for i in range(n - 2, -1, -1):
             if nums[i] == 0: continue
             for take in range(1, nums[i] + 1):
+                if i + take > n - 1: 
+                    break
                 tmp = ans[i + take]
                 if tmp == True: 
                     ans[i] = tmp
                     break
         return ans[0]
+
+#Last executed input:[25000,24999,24998,24997,24996,24995,24994,24993,24992,24991,...
