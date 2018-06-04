845b947353b05e3f279f5f4f1a986a07d55119d3
listenviolet <listenviolet@gmail.com>
2018-06-04 09:33:10 +0800

    use sorted_nums ind - time limit exceeded

--- leetcode/315-Count-of-Smaller-Numbers-After-Self.py
+++ leetcode/315-Count-of-Smaller-Numbers-After-Self.py
@@ -19,6 +19,23 @@ class Solution:
             sorted_nums = sorted_nums[:ind] + sorted_nums[ind + 1:]
         return count
 
+# Description:
+# You are given an integer array nums 
+# and you have to return a new counts array. 
+# The counts array has the property 
+# where counts[i] is the number of smaller elements 
+# to the right of nums[i].
+
+# Example:
+
+# Input: [5,2,6,1]
+# Output: [2,1,1,0] 
+# Explanation:
+# To the right of 5 there are 2 smaller elements (2 and 1).
+# To the right of 2 there is only 1 smaller element (1).
+# To the right of 6 there is 1 smaller element (1).
+# To the right of 1 there is 0 smaller element.
+
 # Solution:
 # 先对nums排序
 # 例如
@@ -45,8 +62,6 @@ class Solution:
 # 0 1 2 3 4 5
 # 5 1 8 3 2 7  -> nums
 # 1 2 3 7 8  -> sorted_nums
-
-
 
 # Time Limit Exceeded
 # 15 / 16 test cases passed.
