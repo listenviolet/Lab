4f2f5775c65517bef435ea8074c281654f5f4d9e
listenviolet <listenviolet@gmail.com>
2018-07-21 10:31:36 +0800

    621-task-scheduler

--- leetcode/621-test-dict-sorted.py
+++ leetcode/621-test-dict-sorted.py
@@ -48,3 +48,32 @@ def leastInterval(tasks, n):
 tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"]
 n = 2
 print(leastInterval(tasks, n))
+
+# 自己测试是正确的，但是提交时显示的结果每次是随机的
+
+# Description:
+# Given a char array representing tasks CPU need to do. 
+# It contains capital letters A to Z 
+# where different letters represent different tasks.
+# Tasks could be done without original order. 
+# Each task could be done in one interval. 
+# For each interval, CPU could finish one task or just be idle.
+
+# However, there is a non-negative cooling interval n 
+# that means between two same tasks, 
+# there must be at least n intervals 
+# that CPU are doing different tasks or just be idle.
+
+# You need to return the least number of intervals 
+# the CPU will take to finish all the given tasks.
+
+# Example 1:
+# Input: tasks = ["A","A","A","B","B","B"], n = 2
+# Output: 8
+# Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+# Note:
+# The number of tasks is in the range [1, 10000].
+# The integer n is in the range [0, 100].
+
+# Solution:
+# 转成dict，每次按任务数从大到小从dict中取任务
